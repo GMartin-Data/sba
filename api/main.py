d615dbd1da107e968798aac794620e89220f0b05
GMartin-Data <gregory.martin.data@gmail.com>
2024-03-04 16:00:33 +0100

    Begin changes to include SHAP values within API response

--- api/main.py
+++ api/main.py
@@ -1,8 +1,12 @@
+import base64
+from io import BytesIO
+
 from fastapi import FastAPI
 import pandas as pd
 from pydantic import BaseModel
+import shap
 
-from model_utils import load_model, predict
+from model_utils import load_model_and_explainer, predict
 
 
 app = FastAPI()
@@ -32,13 +36,18 @@ class FeaturesInput(BaseModel):
 class PredictionOutput(BaseModel):
     category: str
     probability: float
+    shap_plot: str
 
-model = load_model()
+model = load_model_and_explainer()
 
 @app.post("/predict")
 def prediction_route(feature_input: FeaturesInput):
     # feats have here to be a pandas DataFrame
     # in order for the ColumnTransformer to properly work
     inputs = pd.DataFrame(feature_input.model_dump(), index=[0])
-    pred, prob = predict(model, inputs)
+    pred, prob, shap_values = predict(model, inputs)
+
+    # Create the SHAP Waterfall plot
+    shap.plots.waterfall(shap_values[0], show=False)
+
     return PredictionOutput(category=pred, probability=prob)
--- api/model_utils.py
+++ api/model_utils.py
@@ -4,12 +4,18 @@ import joblib
 import pandas as pd
 
 
-def load_model(path: str = "model.joblib"):
-    model = joblib.load(path)
-    return model
+def load_model_and_explainer(model_path: str = "model.joblib", explainer_path: str = "explainer.joblib"):
+    model = joblib.load(model_path)
+    explainer = joblib.load(explainer_path)
+    return model, explainer
 
-def predict(model, data: pd.DataFrame) -> Tuple[str, float]:
+def predict(model, explainer, data: pd.DataFrame) -> Tuple[str, float, 'shap_values']:
     verdict = ["Rejected", "Approved"]
+    # Predicted Class
     pred = model.predict(data)[0]
+    # Associated Probability
     prob = round(model.predict_proba(data)[0][pred] * 100, 2)
-    return verdict[pred], prob
+    # Generate SHAP values
+    shap_values = explainer(data)
+
+    return verdict[pred], prob, shap_values
