30c5fba2a8ec67cc3cac68822fefffaa242df200
GMartin-Data <gregory.martin.data@gmail.com>
2024-03-11 19:30:36 +0100

    Reorganize imports, extract constants and utility functions, clean prints

--- web/prediction/views.py
+++ web/prediction/views.py
@@ -1,12 +1,83 @@
+import datetime
 import json
 import os
 
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
 from requests import Request, Session
 from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
-from django.shortcuts import render
-import datetime
+
 from .forms import LoanApplicationForm
+
+
+REGIONS = {
+    "AL": "Southeast",
+    "AK": "Northwest",
+    "AZ": "Southwest",
+    "AR": "Southeast",
+    "CA": "Southwest",
+    "CO": "Southwest",
+    "CT": "Northeast",
+    'DC': 'Northeast',
+    "DE": "Northeast",
+    "FL": "Southeast",
+    "GA": "Southeast",
+    "HI": "Northwest",
+    "ID": "Northwest",
+    "IL": "Northeast",
+    "IN": "Northeast",
+    "IA": "Northwest",
+    "KS": "Northwest",
+    "KY": "Southeast",
+    "LA": "Southeast",
+    "ME": "Northeast",
+    "MD": "Northeast",
+    "MA": "Northeast",
+    "MI": "Northeast",
+    "MN": "Northwest",
+    "MS": "Southeast",
+    "MO": "Northwest",
+    "MT": "Northwest",
+    "NE": "Northwest",
+    "NV": "Northwest",
+    "NH": "Northeast",
+    "NJ": "Northeast",
+    "NM": "Southwest",
+    "NY": "Northeast",
+    "NC": "Southeast",
+    "ND": "Northwest",
+    "OH": "Northeast",
+    "OK": "Southwest",
+    "OR": "Northwest",
+    "PA": "Northeast",
+    "RI": "Northeast",
+    "SC": "Southeast",
+    "SD": "Northwest",
+    "TN": "Southeast",
+    "TX": "Southwest",
+    "UT": "Northwest",
+    "VT": "Northeast",
+    "VA": "Southeast",
+    "WA": "Northwest",
+    "WV": "Southeast",
+    "WI": "Northwest",
+    "WY": "Northwest"
+}
+
+RECESSION_PERIODS = [
+    (1969, 1970),
+    (1973, 1975),
+    (1980, 1980),
+    (1981, 1982),
+    (1990, 1991),
+    (2001, 2001),
+    (2007, 2009),
+    (2020, 2020),  # Including the COVID-19 pandemic recession
+]
+
+def is_year_in_recession(year: int) -> bool:
+    """Determine if a year is within a recession period or not"""
+    return any(start <= year <= end for start, end in RECESSION_PERIODS)
 
 
 @login_required
@@ -14,101 +85,26 @@ def predict_api_page(request):
     url = os.getenv("URL_API")
     session = Session()
 
-    # TEMPLATE TO BE ADAPTED
     # If this is a POST request we need to process the form data
     if request.method == "POST":
         # Create a form instance and populate it with data from the request
         form = LoanApplicationForm(request.POST)
         # Check whether it's valid or not
         if form.is_valid():
-            form.save()
-
-            regions = {
-                "AL": "Southeast",
-                "AK": "Northwest",
-                "AZ": "Southwest",
-                "AR": "Southeast",
-                "CA": "Southwest",
-                "CO": "Southwest",
-                "CT": "Northeast",
-                "DE": "Northeast",
-                "FL": "Southeast",
-                "GA": "Southeast",
-                "HI": "Northwest",
-                "ID": "Northwest",
-                "IL": "Northeast",
-                "IN": "Northeast",
-                "IA": "Northwest",
-                "KS": "Northwest",
-                "KY": "Southeast",
-                "LA": "Southeast",
-                "ME": "Northeast",
-                "MD": "Northeast",
-                "MA": "Northeast",
-                "MI": "Northeast",
-                "MN": "Northwest",
-                "MS": "Southeast",
-                "MO": "Northwest",
-                "MT": "Northwest",
-                "NE": "Northwest",
-                "NV": "Northwest",
-                "NH": "Northeast",
-                "NJ": "Northeast",
-                "NM": "Southwest",
-                "NY": "Northeast",
-                "NC": "Southeast",
-                "ND": "Northwest",
-                "OH": "Northeast",
-                "OK": "Southwest",
-                "OR": "Northwest",
-                "PA": "Northeast",
-                "RI": "Northeast",
-                "SC": "Southeast",
-                "SD": "Northwest",
-                "TN": "Southeast",
-                "TX": "Southwest",
-                "UT": "Northwest",
-                "VT": "Northeast",
-                "VA": "Southeast",
-                "WA": "Northwest",
-                "WV": "Southeast",
-                "WI": "Northwest",
-                "WY": "Northwest", 
-                'DC' : 'Northeast'
-            }
-
-            recession_periods = [
-                (1969, 1970),
-                (1973, 1975),
-                (1980, 1980),
-                (1981, 1982),
-                (1990, 1991),
-                (2001, 2001),
-                (2007, 2009),
-                (2020, 2020),  # Including the COVID-19 pandemic recession
-            ]
-
-            # Function to check if a given year (from dt.year) was in recession
-            def is_year_in_recession(year):
-                # The year parameter is expected to be an integer
-                return any(start <= year <= end for start, end in recession_periods)
-        
+            form.save()  # THIS MAY HAVE TO BE MOVED
 
             formulaire = form.cleaned_data  # Transform the form to a dict
-            print(f"{regions[formulaire['State']] = }")
-            print(f"{formulaire['State'] == formulaire['BankState'] = }")
 
             # Update formulaire with created features
             date = datetime.datetime.now()
             new_info = {
-                "Region" : regions[formulaire["State"]],
-                "SameState" : True if formulaire["State"] == formulaire["BankState"] else False,
+                "Region" : REGIONS[formulaire["State"]],
+                "SameState" : formulaire["State"] == formulaire["BankState"],
                 "ApprovalMonth" : date.month,
                 "ApprovalDoW" : date.weekday(),
                 "Recession" : is_year_in_recession(date.year)
                 }
             formulaire.update(new_info)
-            print(f"{formulaire = }")
 
             # Serialize formulaire to send it to the API
             payload = json.dumps(formulaire)
@@ -116,7 +112,6 @@ def predict_api_page(request):
             try:
                 response = session.post(url, data=payload)
                 api_resp = response.json()
-                print(f"{api_resp = }")
                 return render(request, "prediction/predict.html",
                               context={"form": form, "api_resp": api_resp})
             except (ConnectionError, Timeout, TooManyRedirects, KeyError) as e:
