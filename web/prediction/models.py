475a8cf82fdfa8621d1088f5d09d121d5a64acbc
GMartin-Data <gregory.martin.data@gmail.com>
2024-03-11 20:11:49 +0100

    Update models with removing City, LowDow and update choices for RevLineCr

--- web/prediction/models.py
+++ web/prediction/models.py
@@ -1,10 +1,9 @@
 from django.db import models
-import datetime
+
 from .enums import *
 
     
 class LoanApplication(models.Model):
-    # city = models.CharField(max_length=50, null=False, blank=False)
     State = models.CharField(max_length=2, choices=USState.choices(), null=False, blank=False)
     Bank = models.CharField(max_length=50, null=False, blank=False)
     BankState = models.CharField(max_length=2, choices=USState.choices(), null=False, blank=False)
@@ -16,7 +15,6 @@ class LoanApplication(models.Model):
     RetainedJob = models.IntegerField(null=False, blank=False)
     Franchise = models.CharField(max_length=1, choices=YesNo.choices(), null=False, blank=False)
     UrbanRural = models.CharField(max_length=1, choices=UrbanRural.choices(), null=False, blank=False)
-    RevLineCr = models.CharField(max_length=7, choices=RevLineCr.choices(), null=False, blank=False)
-    LowDoc = models.CharField(max_length=1, choices=YesNo.choices(), null=False, blank=False)
+    RevLineCr = models.CharField(max_length=7, choices=YesNo.choices(), null=False, blank=False)
     GrAppv = models.FloatField(null=False, blank=False)
     SBA_Appv = models.FloatField(null=False, blank=False)
