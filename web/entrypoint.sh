e2c468bbeaa4b0182c1958ed4b94cab9a10cfd89
GMartin-Data <gregory.martin.data@gmail.com>
2024-03-11 16:11:17 +0100

    Add gunicorn (and collecstatic) to web entrypoint

--- web/entrypoint.sh
+++ web/entrypoint.sh
@@ -4,4 +4,8 @@
 echo "Effectuer les migrations de la base de données..."
 python manage.py makemigrations && python manage.py migrate
 
-python manage.py runserver 0.0.0.0:8000
+echo "Collecter les fichiers statiques"
+python manage.py collectstatic --no-input
+
+echo "Lancer le serveur"
+gunicorn project.wsgi:application --workers=4 --timeout 120 --bind=0.0.0.0:8000
