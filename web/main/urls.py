4338727c30dd11cd59cc8f4b678814d3ebd17fe6
GMartin-Data <gregory.martin.data@gmail.com>
2024-03-12 10:22:21 +0100

    Update urls

--- web/main/urls.py
+++ web/main/urls.py
@@ -1,18 +1,10 @@
-from django.urls import path, include
+from django.urls import path
+
 from . import views
-from project import *
 
 
 urlpatterns = [
     path("", views.home, name="home"),
     path("about/", views.about, name="about"),
-    path("signup/", views.SignupPage.as_view(), name="signup"),
-    path('approve/<int:user_id>/', views.approve_user, name='approve_user'),  # Définir l'URL pour la vue approve_user
-    path("special/", views.special_page, name="special"),
-    path('predict/', include('prediction.urls'), name="pred"),
-    path('eda/', views.eda, name="eda"),
-    path('model/', views.model, name="model"),
-    path('predict/', views.predict_page, name="predict"),
-    path('', views.graphique_interactif, name="graphique_interactif"),
-    path('', views.custom_logout, name="custom_logout")
+    path("signup/", views.SignupView.as_view(), name="signup"),
 ]
