c20b500382db7b733606971ef146ddc5243d7ecb
GMartin-Data <gregory.martin.data@gmail.com>
2024-03-12 10:12:53 +0100

    Add minimalist User class

--- web/main/models.py
+++ web/main/models.py
@@ -1,9 +1,6 @@
-from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-class User(AbstractUser):
-    # Ajouter le champ is_approved
-    is_approved = models.BooleanField(default=False)
 
+class User(AbstractUser):
     def __str__(self):
         return self.username
