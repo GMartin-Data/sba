4a87d466b9816af671940c3b88304f1aecce4e09
GMartin-Data <gregory.martin.data@gmail.com>
2024-03-12 11:18:35 +0100

    Remove 'feats' folder

--- web/feats/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-
-urlpatterns = [
-    path('', views.FeaturesListView.as_view(), name='feat_list'),
-    path('detail/<int:pk>', views.FeaturesDetailView.as_view(), name='feat_detail'),
-]
