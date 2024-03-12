e28edce77d46643a43b3423186b202a7413fa1f1
GMartin-Data <gregory.martin.data@gmail.com>
2024-03-12 10:30:19 +0100

    Fix typo on SignupView (not SignupPage)

--- web/main/views.py
+++ web/main/views.py
@@ -5,7 +5,7 @@ from django.views.generic import CreateView
 from .forms import CustomUserCreationForm
 
 
-class SignupPage(CreateView):
+class SignupView(CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
