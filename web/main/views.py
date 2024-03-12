7ef2b4e28af21f18b7a01767faa8d169005bb3da
GMartin-Data <gregory.martin.data@gmail.com>
2024-03-12 10:28:01 +0100

    Fix unwilling removal of 'home' and 'about' views

--- web/main/views.py
+++ web/main/views.py
@@ -1,10 +1,18 @@
+from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 
 from .forms import CustomUserCreationForm
 
 
-class SignupView(CreateView):
+class SignupPage(CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
+
+
+def home(request):
+    return render(request, 'main/home.html')
+
+def about(request):
+    return render(request, 'main/about.html')
