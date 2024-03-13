ae83f54a36810d1728bc6631f5804c152983afb8
GMartin-Data <gregory.martin.data@gmail.com>
2024-03-13 12:14:59 +0100

    Update links on navbar

--- web/main/context_processors.py
+++ web/main/context_processors.py
@@ -1,8 +1,5 @@
 def global_context(request):
     return {
         "Link1": "About Us",
-        "Link2": "Data Analysis",
-        "Link3": "Machine Learning",
-        "Link4": "Make Predictions",
-        "clicked": "text-white rounded px-2 bg-sba-dark-blue",
+        "Link2": "Make Predictions",
     }
