602b764df03c1ba9c65d6c6a60a3ad931f4162e1
Thibaut W <tibo7@gmail.com>
2024-03-13 09:46:42 +0100

    update navbar links

--- web/main/context_processors.py
+++ web/main/context_processors.py
@@ -1,8 +1,6 @@
 def global_context(request):
     return {
         "Link1": "About Us",
-        "Link2": "Data Analysis",
-        "Link3": "Machine Learning",
-        "Link4": "Make Predictions",
+        "Link2": "Make Predictions",
         "clicked": "text-white rounded px-2 bg-sba-dark-blue",
     }
