5d3761a8834e9381b7bc7c402f145eadc50686be
GMartin-Data <gregory.martin.data@gmail.com>
2024-03-13 10:56:25 +0100

    Wait, checking with netcat, until POSTGRES is launched

--- web/entrypoint.sh
+++ web/entrypoint.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+while ! nc -z $POSTGRES_HOST $POSTGRES_PORT; do
+  echo "En attente de la disponibilité de Postgres..."
+  sleep 1
+done
 
 # Effectuer les migrations
 echo "Effectuer les migrations de la base de données..."
@@ -10,5 +14,6 @@ python manage.py collectstatic --no-input
 echo "Lancer le serveur"
 # Prod
 # gunicorn project.wsgi:application --workers=4 --timeout 120 --bind=0.0.0.0:8000
+
 # Dev
 python manage.py runserver 0.0.0.0:8000
